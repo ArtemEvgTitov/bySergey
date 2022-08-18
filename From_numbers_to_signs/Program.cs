14577c999fbdb498624f8f86a03cccc90ee2586c
ArtemEvgTitov <>
2022-08-18 21:11:23 +0300

    Написан метод создания двумерного массива

--- From_numbers_to_signs/Program.cs
+++ From_numbers_to_signs/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberOfDigits = number.ToString().Length;
+
+char[,] CreateOutputArray(int numberOfDigits)
+{
+    int columns = 6*numberOfDigits;
+    char[,] outputArray = new char [5,columns];
+
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < columns; j++)
+            outputArray[i, j] = ' ';
+    return outputArray;
+} 
+
+char[,] outputArray = CreateOutputArray(numberOfDigits);
+
+void PrintArray(char[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] +" ");
+        Console.WriteLine();
+    }
+}
+
+PrintArray(outputArray);
