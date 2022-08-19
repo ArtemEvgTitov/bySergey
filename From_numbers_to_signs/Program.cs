c5abbfb9051bcc2de44cb9de62a19876e225d3e0
ArtemEvgTitov <>
2022-08-19 11:57:31 +0300

    Укорочен код

--- From_numbers_to_signs/Program.cs
+++ From_numbers_to_signs/Program.cs
@@ -12,7 +12,6 @@ char[,] CreateOutputArray(int numberOfDigits)
 {
     int columns = 6 * numberOfDigits;
     char[,] outputArray = new char[5, columns];
-
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < columns; j++)
             outputArray[i, j] = ' ';
@@ -24,9 +23,7 @@ void PrintArray(char[,] array)
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-        {
             Console.Write(array[i, j]);
-        }
         Console.WriteLine();
     }
 }
@@ -83,156 +80,69 @@ char[,] FillingArray(int numberOfDigits, long number, char[,] outputArray, char[
 char[,] outputArray = CreateOutputArray(numberOfDigits);
 
 char symbol = '@';
+
 char[,] zero = new char[5, 6];
 zero = NumberArray(zero);
-zero[0, 3] = symbol;
-zero[0, 4] = symbol;
-zero[1, 1] = symbol;
-zero[1, 5] = symbol;
-zero[2, 1] = symbol;
-zero[2, 5] = symbol;
-zero[3, 1] = symbol;
-zero[3, 5] = symbol;
-zero[4, 3] = symbol;
-zero[4, 4] = symbol;
+zero[0, 3] = symbol; zero[0, 4] = symbol; zero[2, 1] = symbol; zero[2, 5] = symbol;
+zero[1, 1] = symbol; zero[1, 5] = symbol; zero[3, 1] = symbol; zero[3, 5] = symbol;
+zero[4, 3] = symbol; zero[4, 4] = symbol;
 
 char[,] one = new char[5, 6];
 one = NumberArray(one);
-one[0, 4] = symbol;
-one[1, 3] = symbol;
-one[1, 4] = symbol;
-one[2, 2] = symbol;
-one[2, 4] = symbol;
-one[3, 4] = symbol;
-one[4, 4] = symbol;
+one[0, 4] = symbol; one[1, 3] = symbol; one[1, 4] = symbol; one[2, 2] = symbol;
+one[2, 4] = symbol; one[3, 4] = symbol; one[4, 4] = symbol;
 
 char[,] two = new char[5, 6];
 two = NumberArray(two);
-two[0, 2] = symbol;
-two[0, 3] = symbol;
-two[0, 4] = symbol;
-two[1, 1] = symbol;
-two[1, 4] = symbol;
-two[2, 3] = symbol;
-two[3, 2] = symbol;
-two[4, 1] = symbol;
-two[4, 2] = symbol;
-two[4, 3] = symbol;
-two[4, 4] = symbol;
+two[0, 2] = symbol; two[0, 3] = symbol; two[0, 4] = symbol; two[1, 1] = symbol;
+two[1, 4] = symbol; two[2, 3] = symbol; two[3, 2] = symbol; two[4, 1] = symbol;
+two[4, 2] = symbol; two[4, 3] = symbol; two[4, 4] = symbol;
 
 char[,] three = new char[5, 6];
 three = NumberArray(three);
-three[0, 1] = symbol;
-three[0, 2] = symbol;
-three[0, 3] = symbol;
-three[0, 4] = symbol;
-three[1, 4] = symbol;
-three[2, 2] = symbol;
-three[2, 3] = symbol;
-three[2, 4] = symbol;
-three[3, 4] = symbol;
-three[4, 1] = symbol;
-three[4, 2] = symbol;
-three[4, 3] = symbol;
+three[0, 1] = symbol; three[0, 2] = symbol; three[0, 3] = symbol; three[0, 4] = symbol;
+three[1, 4] = symbol; three[2, 2] = symbol; three[2, 3] = symbol; three[2, 4] = symbol;
+three[3, 4] = symbol; three[4, 1] = symbol; three[4, 2] = symbol; three[4, 3] = symbol;
 three[4, 4] = symbol;
 
 char[,] four = new char[5, 6];
 four = NumberArray(four);
-four[0, 1] = symbol;
-four[0, 4] = symbol;
-four[1, 1] = symbol;
-four[1, 4] = symbol;
-four[2, 1] = symbol;
-four[2, 2] = symbol;
-four[2, 3] = symbol;
-four[2, 4] = symbol;
-four[3, 4] = symbol;
-four[4, 4] = symbol;
+four[0, 1] = symbol; four[0, 4] = symbol; four[1, 1] = symbol; four[1, 4] = symbol;
+four[2, 1] = symbol; four[2, 2] = symbol; four[2, 3] = symbol; four[2, 4] = symbol;
+four[3, 4] = symbol; four[4, 4] = symbol;
 
 char[,] five = new char[5, 6];
 five = NumberArray(five);
-five[0, 1] = symbol;
-five[0, 2] = symbol;
-five[0, 3] = symbol;
-five[0, 4] = symbol;
-five[1, 1] = symbol;
-five[2, 1] = symbol;
-five[2, 2] = symbol;
-five[2, 3] = symbol;
-five[2, 4] = symbol;
-five[3, 4] = symbol;
-five[4, 1] = symbol;
-five[4, 2] = symbol;
-five[4, 3] = symbol;
-five[4, 4] = symbol;
+five[0, 1] = symbol; five[0, 2] = symbol; five[0, 3] = symbol; five[0, 4] = symbol;
+five[1, 1] = symbol; five[2, 1] = symbol; five[2, 2] = symbol; five[2, 3] = symbol;
+five[2, 4] = symbol; five[3, 4] = symbol; five[4, 1] = symbol; five[4, 2] = symbol;
+five[4, 3] = symbol; five[4, 4] = symbol;
 
 char[,] six = new char[5, 6];
 six = NumberArray(six);
-six[0, 1] = symbol;
-six[0, 2] = symbol;
-six[0, 3] = symbol;
-six[0, 4] = symbol;
-six[1, 1] = symbol;
-six[2, 1] = symbol;
-six[2, 2] = symbol;
-six[2, 3] = symbol;
-six[2, 4] = symbol;
-six[3, 1] = symbol;
-six[3, 4] = symbol;
-six[4, 1] = symbol;
-six[4, 2] = symbol;
-six[4, 3] = symbol;
-six[4, 4] = symbol;
+six[0, 1] = symbol; six[0, 2] = symbol; six[0, 3] = symbol; six[0, 4] = symbol;
+six[1, 1] = symbol; six[2, 1] = symbol; six[2, 2] = symbol; six[2, 3] = symbol;
+six[2, 4] = symbol; six[3, 1] = symbol; six[3, 4] = symbol; six[4, 1] = symbol;
+six[4, 2] = symbol; six[4, 3] = symbol; six[4, 4] = symbol;
 
 char[,] seven = new char[5, 6];
 seven = NumberArray(seven);
-seven[0, 1] = symbol;
-seven[0, 2] = symbol;
-seven[0, 3] = symbol;
-seven[0, 4] = symbol;
-seven[1, 3] = symbol;
-seven[2, 2] = symbol;
-seven[3, 2] = symbol;
-seven[4, 2] = symbol;
+seven[0, 1] = symbol; seven[0, 2] = symbol; seven[0, 3] = symbol; seven[0, 4] = symbol;
+seven[1, 3] = symbol; seven[2, 2] = symbol; seven[3, 2] = symbol; seven[4, 2] = symbol;
 
 char[,] eight = new char[5, 6];
 eight = NumberArray(eight);
-eight[0, 1] = symbol;
-eight[0, 2] = symbol;
-eight[0, 3] = symbol;
-eight[0, 4] = symbol;
-eight[1, 1] = symbol;
-eight[1, 4] = symbol;
-eight[2, 1] = symbol;
-eight[2, 2] = symbol;
-eight[2, 3] = symbol;
-eight[2, 4] = symbol;
-eight[3, 1] = symbol;
-eight[3, 4] = symbol;
-eight[4, 1] = symbol;
-eight[4, 2] = symbol;
-eight[4, 3] = symbol;
-eight[4, 4] = symbol;
+eight[0, 1] = symbol; eight[0, 2] = symbol; eight[0, 3] = symbol; eight[0, 4] = symbol;
+eight[1, 1] = symbol; eight[1, 4] = symbol; eight[2, 1] = symbol; eight[2, 2] = symbol;
+eight[2, 3] = symbol; eight[2, 4] = symbol; eight[3, 1] = symbol; eight[3, 4] = symbol;
+eight[4, 1] = symbol; eight[4, 2] = symbol; eight[4, 3] = symbol; eight[4, 4] = symbol;
 
 char[,] nine = new char[5, 6];
 nine = NumberArray(nine);
-nine[0, 1] = symbol;
-nine[0, 2] = symbol;
-nine[0, 3] = symbol;
-nine[0, 4] = symbol;
-nine[1, 1] = symbol;
-nine[1, 4] = symbol;
-nine[2, 1] = symbol;
-nine[2, 2] = symbol;
-nine[2, 3] = symbol;
-nine[2, 4] = symbol;
-nine[3, 4] = symbol;
-nine[4, 1] = symbol;
-nine[4, 2] = symbol;
-nine[4, 3] = symbol;
-nine[4, 4] = symbol;
-
+nine[0, 1] = symbol; nine[0, 2] = symbol; nine[0, 3] = symbol; nine[0, 4] = symbol;
+nine[1, 1] = symbol; nine[1, 4] = symbol; nine[2, 1] = symbol; nine[2, 2] = symbol;
+nine[2, 3] = symbol; nine[2, 4] = symbol; nine[3, 4] = symbol; nine[4, 1] = symbol;
+nine[4, 2] = symbol; nine[4, 3] = symbol; nine[4, 4] = symbol;
 
 outputArray = FillingArray(numberOfDigits, number, outputArray, zero, one, two, three, four, five, six, seven, eight, nine);
-
 PrintArray(outputArray);
